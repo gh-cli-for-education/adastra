085800263d98cb98401b33e0b27887cc936d7976
Jaime <alu0101329093@ull.edu.es>
2023-05-07 01:26:57 +0100

    feat: add tailwind config from adastra config

--- templates/basic/tailwind.config.cjs
+++ templates/basic/tailwind.config.cjs
@@ -1,8 +1,9 @@
+const { tailwindConfig } = require("../adastra.config.cjs");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  presets: [tailwindConfig],
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {},
+  plugins: [],
+};
